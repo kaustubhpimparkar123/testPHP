9fe85e5de6e30dfcb33d4af8120032c67510c9e5
kaustubhpimparkar123 <33192876+kaustubhpimparkar123@users.noreply.github.com>
2019-08-22 11:30:07 +0530

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="yellow">
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+</body>
+</html>
